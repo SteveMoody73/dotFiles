9f60ca15e4f22c78c72bced14a230c5441334e7b
Stephen Moody <steve.moody73@gmail.com>
2017-04-12 11:57:00 +0200

    Updated vim build instructions

--- vim-with-lua.sh
+++ vim-with-lua.sh
@@ -1,38 +1,34 @@
 #!/bin/sh
 
-sudo apt-get remove --purge vim vim-runtime vim-gnome vim-tiny vim-common vim-gui-common
-sudo apt-get install liblua5.1-dev luajit libluajit-5.1 python-dev ruby-dev libperl-dev mercurial libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev
+sudo apt-get remove --purge vim vim-runtime vim-gnome vim-tiny vim-gui-common
 
-sudo mkdir /usr/include/lua5.1/include
-sudo ln -s /usr/include/luajit-2.0 /usr/include/lua5.1/include
- 
-git clone https://github.com/vim/vim.git
+sudo apt-get install liblua5.2-dev luajit libluajit-5.2 python-dev ruby-dev libperl-dev libncurses5-dev libatk1.0-dev libx11-dev libxpm-dev libxt-dev
+
+sudo rm -rf /usr/local/share/vim
+
+sudo rm /usr/bin/vim
+
+sudo mkdir /usr/include/lua5.2/include
+sudo cp /usr/include/lua5.2/*.h /usr/include/lua5.2/include/
+
+git clone https://github.com/vim/vim
+#git pull && git fetch
 cd vim/src
-
-#cd vim
-#git reset --hard
-#git pull
-#cd src
-
-make distclean
+make distclean # if vim was prev installed
 ./configure --with-features=huge \
             --enable-rubyinterp \
             --enable-largefile \
             --disable-netbeans \
             --enable-pythoninterp \
-            --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
+            --with-python-config-dir=/usr/lib/python2.7/config \
             --enable-perlinterp \
             --enable-luainterp \
             --with-luajit \
-            --enable-gui=auto \
             --enable-fail-if-missing \
-            --with-lua-prefix=/usr/include/lua5.1 \
-            --enable-cscope 
-make 
+            --with-lua-prefix=/usr/include/lua5.2 \
+            --enable-cscope
 
+make 
 sudo make install
 
-cd ..
-sudo mkdir /usr/share/vim
-sudo mkdir /usr/share/vim/vim74
-sudo cp -fr runtime/* /usr/share/vim/vim74/
+# alias vi="vim" in .zshrc / .bashrc
