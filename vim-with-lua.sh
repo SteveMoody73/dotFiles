618574471657b32a3f3b58daddbdf7d6c18cd20d
Stephen Moody <steve.moody73@gmail.com>
2017-08-23 11:08:19 +0200

    Update vim build script

--- vim-with-lua.sh
+++ vim-with-lua.sh
@@ -1,34 +1,32 @@
-#!/bin/sh
+# Remove previous installations
+sudo apt-get remove vim vim-runtime vim-tiny vim-common
 
-sudo apt-get remove --purge vim vim-runtime vim-gnome vim-tiny vim-gui-common
+# Install dependencies
+sudo apt-get install libncurses5-dev python-dev libperl-dev ruby-dev liblua5.2-dev
 
-sudo apt-get install liblua5.1-dev luajit libluajit-5.1 python-dev ruby-dev libperl-dev libncurses5-dev libatk1.0-dev libx11-dev libxpm-dev libxt-dev
+# Fix liblua paths
+sudo ln -s /usr/include/lua5.2 /usr/include/lua
+sudo ln -s /usr/lib/x86_64-linux-gnu/liblua5.2.so /usr/local/lib/liblua.so
 
-sudo rm -rf /usr/local/share/vim
+# Clone vim sources
+cd ~
+git clone https://github.com/vim/vim.git
 
-sudo rm /usr/bin/vim
+cd vim
+./configure --prefix=/usr     \
+    --enable-luainterp=yes    \
+    --enable-perlinterp=yes   \
+    --enable-pythoninterp=yes \
+    --enable-rubyinterp=yes   \
+    --enable-cscope           \
+    --disable-netbeans        \
+    --enable-multibyte        \
+    --enable-largefile        \
+    --enable-gui=no           \
+    --with-features=huge      \
+    --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu
 
-sudo mkdir /usr/include/lua5.1/include
-sudo cp /usr/include/lua5.1/*.h /usr/include/lua5.1/include/
+make VIMRUNTIMEDIR=/usr/share/vim/vim74
 
-git clone https://github.com/vim/vim
-#git pull && git fetch
-cd vim/src
-make distclean # if vim was prev installed
-./configure --with-features=huge \
-            --enable-rubyinterp \
-            --enable-largefile \
-            --disable-netbeans \
-            --enable-pythoninterp \
-            --with-python-config-dir=/usr/lib/python2.7/config \
-            --enable-perlinterp \
-            --enable-luainterp \
-            --with-luajit \
-            --enable-fail-if-missing \
-            --with-lua-prefix=/usr/include/lua5.1 \
-            --enable-cscope
-
-make 
-sudo make install
-
-# alias vi="vim" in .zshrc / .bashrc
+sudo apt-get install checkinstall
+sudo checkinstall
